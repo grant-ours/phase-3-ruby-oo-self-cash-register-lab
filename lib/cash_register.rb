e51f83e49174ae8a550cc4dd60f8b79d0185d0e0
Grant Ours <grant.ours55@gmail.com>
2022-08-31 16:36:52 -0600

    commit tree ee51ba560a9765e1bfc8ff816cdcd09f1bdf0bbb

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,34 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :items, :last_trasnaction
+    attr_reader :discount
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @money_off = 0
+        @items = []
+        @previous_total = []
+    end
+    def add_item(title, price, quantity=1)
+        @previous_total = @total
+        @total += price*quantity
+        quantity.times{@items << title}
+    end
+    def apply_discount
+        if(@discount == 0)
+            "There is no discount to apply."
+        else
+            @discount = @total * (@discount.to_f / 100)
+            @total -= @discount.to_i
+            "After the discount, the total comes to $#{@total}."
+        end
+    end
+    def items
+        @items
+    end
+    def void_last_transaction
+        @total = @previous_total
+    end
+end
+ binding.pry
